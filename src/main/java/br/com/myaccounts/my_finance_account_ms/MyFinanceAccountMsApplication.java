5a66e0359474e9ec9e730768c9b6608c05804d9c
Guilherme Mello <guilherme.araujo@cilia.com.br>
2025-07-18 16:26:34 -0300

    init

---
+++ src/main/java/br/com/myaccounts/my_finance_account_ms/MyFinanceAccountMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.myaccounts.my_finance_account_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinanceAccountMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinanceAccountMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/myaccounts/my_finance_account_ms/MyFinanceAccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.myaccounts.my_finance_account_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinanceAccountMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
